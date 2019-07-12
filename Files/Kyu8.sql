89d94f92d5ae0f442c7b5a7e63daefb485d0430f
Danielkaas94 <danielkj94@hotmail.com>
2019-07-12 13:27:42 +0200

    SQL File Update - Kyu8.sql New Katas:     - Adults only     - SQL Basics: Mod

--- Files/Kyu8.sql
+++ Files/Kyu8.sql
@@ -22,11 +22,60 @@ WHERE
   
   
 -- 8 Kyu - Keep Hydrated!
+/*
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+Kata: https://www.codewars.com/kata/582cb0224e56e068d800003c
+*/
 SELECT 
   id, hours, FLOOR(hours/2) as liters
 
 FROM 
   cycling
   
+  
 -- 8 Kyu - Keep Hydrated2!
 SELECT *, trunc(hours* 0.5)  AS liters FROM cycling
+  
+  
+  
+  
+  
+  
+-- 8 Kyu - Adults only (SQL for Beginners #1)
+/*
+In your application, there is a section for adults only. You need to get a list of names and ages of users from the users table, who are 18 years old or older.
+
+Kata: https://www.codewars.com/kata/590a95eede09f87472000213
+*/
+SELECT * 
+
+FROM 
+  users
+
+WHERE 
+  age >= 18
+    
+  
+  
+  
+  
+  
+-- 8 Kyu - SQL Basics: Mod
+/*
+Return a table with one column (mod) which is the output of number1 modulus number2.
+
+Kata: https://www.codewars.com/kata/590a95eede09f87472000213
+*/
+SELECT number1 % number2 as mod 
+
+FROM decimals
+
+
+-- 8 Kyu - SQL Basics: Mod2
+SELECT MOD(number1, number2)
+FROM decimals
