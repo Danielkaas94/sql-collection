3011b7a06d9c44367394acc3af1645914cc44925
Danielkaas94 <danielkj94@hotmail.com>
2020-02-18 21:43:43 +0100

    7 Kyu - SQL Basics: Simple JOIN with COUNT
    SELECT
      people.*,
      Count(toys.*) AS toy_count
    
    FROM
      people
    
    LEFT OUTER JOIN
      toys ON toys.people_id = people.id
    
    GROUP BY
      people.id

--- Files/Kyu7.sql
+++ Files/Kyu7.sql
@@ -111,3 +111,24 @@ FROM
   
 ORDER BY 
 	comma
+	
+	
+
+ -- 7 Kyu - SQL Basics: Simple JOIN with COUNT
+/*
+	Create a simple SELECT statement that will return all columns from the people table, 
+	and join to the toys table so that you can return the COUNT of the toys.
+	You should return all people fields as well as the toy count as "toy_count".
+*/
+SELECT 
+  people.*,
+  Count(toys.*) AS toy_count
+
+FROM 
+  people
+  
+LEFT OUTER JOIN 
+  toys ON toys.people_id = people.id
+  
+GROUP BY 
+  people.id
